f3d4139d70ed0832616fab8884dad77176d1435c
Abe <abhi.rawat456@gmail.com>
2020-03-15 16:18:00 +0530

    created basic structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "88469975";
+  const APP_KEY = "207f13595dac5dcd41b76adb5bb09176";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-for,">
+        <input type="text" className="search-bar" id="" />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
