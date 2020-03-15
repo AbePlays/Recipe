3d6df3f24251a6c64f05085e4d26eaee45445ca9
Abe <abhi.rawat456@gmail.com>
2020-03-15 16:34:43 +0530

    added state components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "88469975";
   const APP_KEY = "207f13595dac5dcd41b76adb5bb09176";
 
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    //getRecipes();
+  }, []);
+
   const getRecipes = async () => {
     const response = await fetch(
       `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
-    console.log(data);
+    setRecipes(data.hits);
   };
 
   return (
